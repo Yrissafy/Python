71abf85262e3c70eff2d9c212c9aa085d033b800
Yrissafy <113034145+Yrissafy@users.noreply.github.com>
2022-10-18 09:53:48 +0200

    Add files via upload

---
+++ algo/tp2/python tp2.py
@@ -0,0 +1,106 @@
+# Créé par yasser.rissafy, le 20/09/2022 en Python 3.7
+
+
+"""
+#1
+for n in range(1.16):
+    print(n)
+"""
+
+"""
+#ex2
+for n in range(15.-1.-1):
+    print(n)
+"""
+
+"""
+#ex3
+a=int(input("valeur de debut:"))
+b=int(input("valeur de fin"))
+for n in range(a,b):
+    print(n)
+"""
+
+"""
+#4
+a=int(input("votre valeur de début:"))
+for n in range(a,10):
+    print(n)
+"""
+
+
+"""
+#6
+max=0
+for n in range (5):
+    nb=int(input("saisir un nombre"))
+    if nb>max:
+        max=nb
+
+print ("le nb max est", max)
+"""
+
+"""
+#7
+valeur=0
+ax=0
+while valeur>=0:
+    valeur=int(input("saisir un nombre positif ou -1"))
+    if valeur>max:
+        max=valeur
+
+print("le nb max est",max)
+"""
+
+
+"""
+#8
+valeur=0
+somme=0
+while valeur>=0:
+    print("la somme des nombres est",somme)
+"""
+
+
+"""
+#9
+menu=0
+while menu≠'q':
+    print("1: charger le fichier")
+    print("2: sauvegarder le fichier")
+    print("3: afficher les données")
+    print("4: modifier les donner")
+    print("q: quitter")
+    print("votre choix?")
+if menu=='1':
+    print("chargement")
+elif menu=='2':
+    print("sauvegarde")
+elif menu=='3':
+    print("affichage")
+elif menu=='4':
+    print("modification")
+elif=='q':
+    print("au revoir")
+else:
+    print("erreur")
+"""
+
+
+
+#10
+from random import*
+valeur = 0
+while valeur != nombre:
+     valeur=int(input("votre chiffre"))
+     if valeur>nombre:
+         print("chiffre trop grand")
+     elif valeur<nombre:
+        print("chiffre trop petit")
+else:
+    print("exact")
+
+
+#11
+
+
